89d9bf43271805e7e7f51408b69a405fd42779cd
Eduard Bagdagulyan <eduard_bagdagulyan@epam.com>
2022-12-04 11:22:00 +0400

    finished fs/create.js task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
+import { writeFile } from 'fs'
+import { join } from 'path'
+
 const create = async () => {
-    // Write your code here 
+    const filePath = join('files', 'fresh.txt')
+    const data = 'I am fresh and young'
+
+    writeFile(filePath, data, {flag: 'wx'}, (err) => {
+        if (err && err.code === 'EEXIST') throw new Error('FS operation failed');
+        console.log('Created Successfully!');
+    });
 };
 
 await create();
