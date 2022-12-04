f9ad6743c827cc1003001ef602faaab3772f7e4c
Eduard Bagdagulyan <eduard_bagdagulyan@epam.com>
2022-12-04 12:58:12 +0400

    finished cli/args.js task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2)
+
+    args.forEach((value, index, array) => {
+        if (value.startsWith('--')) {
+            console.log(`${value} is ${array[index + 1]}`)
+        }
+    })
 };
 
 parseArgs();
