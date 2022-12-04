fcd2f5772818617e4bc4681f889b02fd5474614b
Eduard Bagdagulyan <eduard_bagdagulyan@epam.com>
2022-12-04 12:35:54 +0400

    finished fs/read.js task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,13 @@
+import { readFile } from 'fs'
+import { join } from 'path'
+
 const read = async () => {
-    // Write your code here 
+    const fileToReadPath = join('files', 'fileToRead.txt')
+
+    readFile(fileToReadPath, {encoding: 'utf-8'}, (err, data) => {
+        if (err) throw new Error('FS operation failed')
+        console.log(data)
+    })
 };
 
 await read();
