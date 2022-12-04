dc668763a013e7cacbff31b610541796ebcfa102
Eduard Bagdagulyan <eduard_bagdagulyan@epam.com>
2022-12-04 15:54:45 +0400

    finished streams/write.js task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,11 @@
+import { createWriteStream } from "fs";
+import { join } from "path";
+
 const write = async () => {
-    // Write your code here 
+    const filePath = join('files', 'fileToWrite.txt')
+    const file = createWriteStream(filePath)
+    process.stdin.resume()
+    process.stdin.pipe(file)
 };
 
 await write();
