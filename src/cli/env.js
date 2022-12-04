4c714c27e4b9ece7edf23bcd6067cd6895ae503a
Eduard Bagdagulyan <eduard_bagdagulyan@epam.com>
2022-12-04 12:49:22 +0400

    finished cli/env.js task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,7 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envVarKeys = Object.keys(process.env)
+    const validVars = envVarKeys.filter(value => value.startsWith('RSS_'))
+    validVars.forEach(value => console.log(`${value}=${process.env[value]}`))
 };
 
 parseEnv();
