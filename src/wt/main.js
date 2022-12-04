85204de2942ab21ff2240827f24949770570bcbd
Eduard Bagdagulyan <eduard_bagdagulyan@epam.com>
2022-12-04 18:04:34 +0400

    finished wt task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,21 @@
+import { Worker } from 'worker_threads'
+import path from 'path';
+import os from 'os'
+
 const performCalculations = async () => {
-    // Write your code here
+    const workerFilePath = path.resolve('worker.js')
+    const availableCores = os.cpus()
+    const result = []
+
+    availableCores.forEach((value, index) => {
+        result.push(new Promise((resolve, reject) => {
+            const worker = new Worker(workerFilePath, {workerData: 10 + index})
+            worker.on('message', data => resolve({data, status: 'resolved'}))
+            worker.on('error', () => reject({data: null, status: 'error'}))
+        }))
+    })
+
+    console.log(await Promise.all(result));
 };
 
 await performCalculations();
