fc621bebeb51a855733a8c333b9d5e3ac1b63358
Eduard Bagdagulyan <eduard_bagdagulyan@epam.com>
2022-12-04 15:43:25 +0400

    finished hash/calcHash.js task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import { join } from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = join('files', 'fileToCalculateHashFor.txt')
+    const fd = createReadStream(filePath)
+    const hash = createHash('sha256')
+    hash.setEncoding('hex')
+
+    fd.on('end', function() {
+        hash.end();
+        console.log(hash.read());
+    });
+
+    fd.pipe(hash);
 };
 
 await calculateHash();
