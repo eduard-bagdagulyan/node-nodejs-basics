404e1418d2d08f4f3f793d291d0589b23d8a4456
Eduard Bagdagulyan <eduard_bagdagulyan@epam.com>
2022-12-04 16:24:33 +0400

    finished zip/compress.js task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { join } from 'path';
+import { createGzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+
 const compress = async () => {
-    // Write your code here 
+    const initialFilePath = join('files', 'fileToCompress.txt')
+    const compressedFilePath = join('files', 'archive.txt.gz')
+
+    const stream = createReadStream(initialFilePath)
+    stream
+        .pipe(createGzip())
+        .pipe(createWriteStream(compressedFilePath))
+        .on('finish', () => {
+            console.log(`Compressed Successfully!`)
+        })
 };
 
 await compress();
