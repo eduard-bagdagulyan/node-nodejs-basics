9ed9bb241ef1002714477d8b4ae6595c52199ace
Eduard Bagdagulyan <eduard_bagdagulyan@epam.com>
2022-12-04 16:50:48 +0400

    finished zip/decompress.js task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { join } from 'path';
+import { createGunzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+
 const decompress = async () => {
-    // Write your code here 
+    const archivePath = join('files', 'archive.txt.gz')
+    const outputFilePath = join('files', 'fileToCompress.txt')
+
+    const stream = createReadStream(archivePath)
+    stream
+        .pipe(createGunzip())
+        .pipe(createWriteStream(outputFilePath))
+        .on('finish', () => {
+            console.log(`Decompressed Successfully!`)
+        })
 };
 
 await decompress();
