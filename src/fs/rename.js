ef72feb6f4d47b5c97a8a091e7752f74788f7516
Eduard Bagdagulyan <eduard_bagdagulyan@epam.com>
2022-12-04 12:16:28 +0400

    finished fs/rename.js task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,18 @@
+import { join } from 'path'
+import { existsSync, rename as renameFile } from 'fs';
+
 const rename = async () => {
-    // Write your code here 
+    const wrongFilePath = join('files', 'wrongFilename.txt')
+    const properFilePath = join('files', 'properFilename.md')
+
+    if (existsSync(wrongFilePath) && !existsSync(properFilePath)) {
+        renameFile(wrongFilePath, properFilePath, (err) => {
+            if (err) throw new Error('Fs operation failed')
+            console.log('Renamed Successfully')
+        })
+    } else {
+        throw new Error('Fs operation failed')
+    }
 };
 
 await rename();
