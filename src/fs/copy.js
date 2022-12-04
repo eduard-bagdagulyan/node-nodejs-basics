9567c13cc2679851f124d7e1d2f9562c1a40bc49
Eduard Bagdagulyan <eduard_bagdagulyan@epam.com>
2022-12-04 12:04:28 +0400

    finished fs/copy.js task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,17 @@
+import { existsSync, cp } from 'fs';
+
 const copy = async () => {
-    // Write your code here 
+    const filesDirPath = 'files'
+    const filesCopyDirPath = 'files_copy'
+
+    if (existsSync(filesDirPath) && !existsSync(filesCopyDirPath)) {
+        cp(filesDirPath, filesCopyDirPath, {recursive: true}, (err) => {
+            if (err) throw new Error('FS operation failed')
+            console.log('Copied Successfully!')
+        })
+    } else {
+        throw new Error('FS operation failed')
+    }
 };
 
 copy();
